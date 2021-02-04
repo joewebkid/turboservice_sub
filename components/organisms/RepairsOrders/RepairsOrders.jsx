6002ef0ef579760ef94d2d1dea76411c0758fa40
Дима <milkdimka@gmail.com>
2021-02-04 13:19:33 +0300

    1

--- components/organisms/RepairsOrders/RepairsOrders.jsx
+++ components/organisms/RepairsOrders/RepairsOrders.jsx
@@ -257,27 +257,26 @@ const RepairsOrders = (props) => {
                   <td scope="col" colSpan="10">
                     <FlexBlock justify="space-between">
                       <Block>{order["REQUEST_TEXT"]}</Block>
-                      <CustomLink
-                        href={"/order/" + order["WORKORDER_ID"]}
-                        className=" pr-3"
-                      >
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="external-link-alt"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          className="btn btn-link"
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"
-                            class=""
-                          ></path>
-                        </svg>
-                      </CustomLink>
+                      <Block className=" mr-3" style={{ width: 24 }}>
+                        <CustomLink href={"/order/" + order["WORKORDER_ID"]}>
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="external-link-alt"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                            className="btn btn-link"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"
+                              class=""
+                            ></path>
+                          </svg>
+                        </CustomLink>
+                      </Block>
                     </FlexBlock>
                   </td>
                 </tr>
@@ -303,7 +302,7 @@ const RepairsOrders = (props) => {
                 ))}
               </select>
             </Block>
-            entries of {total}
+            entries {total ? <>of {total}</> : <></>}
           </Block>
           <Pagination>
             {pages ? (
