07b93b3baf7fb5b5a1c4318580aa9cd81c92b3ff
Дима <milkdimka@gmail.com>
2021-02-04 13:24:40 +0300

    1

--- components/organisms/RepairsOrders/RepairsOrders.jsx
+++ components/organisms/RepairsOrders/RepairsOrders.jsx
@@ -268,6 +268,7 @@ const RepairsOrders = (props) => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512"
                             className="btn btn-link"
+                            style={{ width: 47 }}
                           >
                             <path
                               fill="currentColor"
